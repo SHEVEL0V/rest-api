2db13cffc2017011703b9d55c1a2192e5cd23bff
Андрій Шевельов <ashevelevmti@gmail.com>
2022-08-03 21:28:20 +0300

    add dotenv

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 /** @format */
 
 const app = require("./src/app");
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
+app.listen(port, () => {
+  console.log(`Server running. Use our API on port: ${port}`);
 });
